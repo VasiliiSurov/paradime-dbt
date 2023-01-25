1c5ea69e5f8fdebc7a57894ae1470baf4332b54c
Vasilii Surov <sova.paradime@gmail.com>
2023-01-25 16:20:55 +0000

    fake change

--- models/analys/geo_station.sql
+++ models/analys/geo_station.sql
@@ -1,7 +1,8 @@
 {{config(materialized='view')}}
 select
-  station_id
-, station_name
-, ST_GEOGPOINT(station_longitude, station_latitude) as station_point
+    station_id,
+    station_name,
+    ST_GEOGPOINT(station_longitude, station_latitude) as station_point,
+    1 as fake column
 from {{ ref('stations') }}
 where station_latitude between 40 and 41
